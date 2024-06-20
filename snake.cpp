9b42562fa22a1927e5019b5a7057e8c5e9b6c082
Hubert Tułacz <htulacz@student.agh.edu.pl>
2024-06-20 11:29:55 +0200

    finito v1

--- snake.cpp
+++ snake.cpp
@@ -3,15 +3,54 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cmath>
-#include <stdlib.h>
+#include <deque>
+#include <cstdlib>
+#include <ctime>
 #include <algorithm>
-#include "options.h"
+#include <unistd.h>
+#include <time.h>
+
 bool run = true;
+bool snakeRun = true;
 
 using namespace std;
 
-int main(){
+struct Position {
+    int x, y;
+
+    bool operator==(const Position &other) const {
+        return x == other.x && y == other.y;
+    }
+};
+
+void drawBorder(int width, int height) {
+    for (int i = 0; i < width; ++i) {
+        mvprintw(0, i, "#");
+        mvprintw(height - 1, i, "#");
+    }
+    for (int i = 0; i < height; ++i) {
+        mvprintw(i, 0, "#");
+        mvprintw(i, width - 1, "#");
+    }
+}
+
+void generateFood(Position &food, int width, int height, const deque<Position> &snake) {
+    bool valid = false;
+    while (!valid) {
+        valid = true;
+        food.x = rand() % (width - 2) + 1;
+        food.y = rand() % (height - 2) + 1;
+        for (const auto &pos : snake) {
+            if (pos == food) {
+                valid = false;
+                break;
+            }
+        }
+    }
+}
+
+int main() {
+    srand(time(0));
 
     ifstream menuFile("MenuOptions.txt");
     if (!menuFile) {
@@ -24,7 +63,7 @@ int main(){
     size_t maxOptionLength = 0;
     while (getline(menuFile, menuOption)) {
         menuOptions.push_back(menuOption);
-        if (menuOption.length() > maxOptionLength){
+        if (menuOption.length() > maxOptionLength) {
             maxOptionLength = menuOption.length();
         }
     }
@@ -36,10 +75,10 @@ int main(){
 
     int input = 0;
     int selected = 1;
-    while (run){
+    while (run) {
         wclear(stdscr);
         mvprintw(1, 12, "Menu");
-        for (size_t i = 0; i < menuOption.size(); i++) {
+        for (size_t i = 0; i < menuOptions.size(); i++) {
             mvprintw(i + 2, 15, "%zu. %s", i + 1, menuOptions[i].c_str());
         }
         if (selected > 0 && selected <= menuOptions.size()) {
@@ -52,10 +91,66 @@ int main(){
         if (input == KEY_UP && selected > 1) selected--;
 
         if (input == 10) {
-            if (selected == 3){
+            if (selected == 3) {
                 run = false;
             } else {
+                int width = 50;
+                int height = 20;
+                Position food;
+                deque<Position> snake = {{width / 2, height / 2}};
+                Position direction = {0, 1};
+                generateFood(food, width, height, snake);
 
+                struct timespec sleepTime;
+                sleepTime.tv_sec = 0;
+                sleepTime.tv_nsec = 500000000L;
+
+                time_t startTime = time(nullptr);
+
+                snakeRun = true;
+                while (snakeRun) {
+                    nodelay(stdscr, TRUE);
+                    input = getch();
+                    switch (input) {
+                        case KEY_UP: if (direction.y != 1) direction = {0, -1}; break;
+                        case KEY_DOWN: if (direction.y != -1) direction = {0, 1}; break;
+                        case KEY_LEFT: if (direction.x != 1) direction = {-1, 0}; break;
+                        case KEY_RIGHT: if (direction.x != -1) direction = {1, 0}; break;
+                    }
+
+                    Position newHead = {snake.front().x + direction.x, snake.front().y + direction.y};
+
+                    if (newHead.x <= 0 || newHead.x >= width - 1 || newHead.y <= 0 || newHead.y >= height - 1 ||
+                        find(snake.begin(), snake.end(), newHead) != snake.end()) {
+                        snakeRun = false;
+                        break;
+                    }
+
+                    snake.push_front(newHead);
+                    if (newHead == food) {
+                        generateFood(food, width, height, snake);
+                    } else {
+                        snake.pop_back();
+                    }
+
+                    wclear(stdscr);
+                    drawBorder(width, height);
+                    mvprintw(food.y, food.x, "*");
+
+                    for (const auto &pos : snake) {
+                        mvprintw(pos.y, pos.x, "O");
+                    }
+
+                    time_t currentTime = time(nullptr);
+                    int elapsedTime = static_cast<int>(difftime(currentTime, startTime));
+                    mvprintw(0, width + 2, "Time: %d sec", elapsedTime);
+
+                    refresh();
+
+                    nanosleep(&sleepTime, nullptr);
+                }
+                timeout(-1);
+                nodelay(stdscr, FALSE);
             }
         }
     }
